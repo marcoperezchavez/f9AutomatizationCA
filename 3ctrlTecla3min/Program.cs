c521489426001a1609797f0b98830f0f72de05f4
Marco Antonio de Jesus Perez Chavez <maperez@tiempodevelopment.com>
2020-12-14 17:17:21 -0600

    first commit

---
+++ 3ctrlTecla3min/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _3ctrlTecla3min
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Clickeara 2 veces la tecla control cada 3 minutos");
+            for (int i = 0; i < 1000; i++)
+            {
+               
+                Thread.Sleep(30000);
+                SendKeys.SendWait("{F9}");
+                SendKeys.SendWait("{F9}");
+            }
+        }
+    }
+}
