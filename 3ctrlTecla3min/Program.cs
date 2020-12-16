de171934745dd608e3949e34910071f15c93c388
Marco Antonio de Jesus Perez Chavez <maperez@tiempodevelopment.com>
2020-12-16 16:12:24 -0600

    added console message

--- 3ctrlTecla3min/Program.cs
+++ 3ctrlTecla3min/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace _3ctrlTecla3min
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
         [STAThread]
         static void Main(string[] args)
         {
-            //Console.WriteLine("Clickeara 2 veces la tecla control cada 3 minutos");
+            AllocConsole();
+            Console.WriteLine("Clickeara 2 veces la tecla F9 cada 3 minutos");
             for (int i = 0; i < 1000; i++)
             {
-               
+                Console.WriteLine($"Ciclo {i+1}");
                 Thread.Sleep(30000);
                 SendKeys.SendWait("{F9}");
                 SendKeys.SendWait("{F9}");
