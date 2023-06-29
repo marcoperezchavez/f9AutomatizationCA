aa7e5b424c1e960299a29cd008535af6477596e6
marcoperezchavez <marcoperezchavez@gmail.com>
2023-06-29 11:14:51 -0500

    added class code

---
+++ Ejecutable vnet7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace _3ctrlTecla3min
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+        [STAThread]
+        static void Main(string[] args)
+        {
+            AllocConsole();
+            Console.WriteLine("Clickeara 2 veces la tecla F9 cada 3 minutos");
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"Ciclo {i+1}");
+                Thread.Sleep(300000);
+                SendKeys.SendWait("{F9}");
+                SendKeys.SendWait("{F9}");
+            }
+        }
+    }
+}
